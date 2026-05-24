0e381df25363bf94ef1e1ef8a70da121cd3aab21
Gabriel Castro <gabrieltcastro@users.noreply.github.com>
2026-05-24 03:41:44 -0300

    feat: update Firebase project ID to mya-oficial

--- js/produtos.js
+++ js/produtos.js
@@ -3,13 +3,23 @@
    Otimizado com rAF throttle e atualizações em tempo real
    ================================================ */
 
-import { firebaseConfig } from './firebase-config.js';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+  // Configuração do Firebase Realtime Database para o Dani Site (Mya)
+  const firebaseConfig = {
+    apiKey: "AIzaSyDmeQRWvfia5U1JZOZPDwM_0apdPo09cpc",
+    authDomain: "mya-oficial.firebaseapp.com",
+    databaseURL: "https://mya-oficial-default-rtdb.firebaseio.com",
+    projectId: "mya-oficial",
+    storageBucket: "mya-oficial.firebasestorage.app",
+    messagingSenderId: "322566791231",
+    appId: "1:322566791231:web:94492e5af7866ca8bf588a"
+  };
+
+  // Inicializa Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
 
 (function() {
   'use strict';
